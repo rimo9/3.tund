3b5961ad4d2ecb4221515fc467dae05e4ee7c969
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2015-09-18 13:39:41 +0300

    added test

--- PAGE/login.php
+++ PAGE/login.php
@@ -10,7 +10,11 @@
 	$repassworderror = "";
 	$create_passwordAerror = "";
 	$create_usererror = "";
-	
+	$email = "";
+	$password = "";
+	$Cpassword = "";
+	$Cusername = "";
+	$Cemail = "";
 	//kontrollin kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		
@@ -20,11 +24,15 @@
 			if(empty($_POST["email"])){
 				//jah oli tühi
 				$email_error = "E-mail is required";
+			}else{
+				$email = test_input($_POST["email"]);
 			}
 			//kas password on tühi
 			if(empty($_POST["password"])){
 				//jah oli tühi
 				$password_error = "Password is required";
+			}else{
+				$password = test_input($_POST["password"]);
 			}
 		}
 		elseif(isset($_POST["Create"])){
@@ -32,27 +40,42 @@
 			if(empty($_POST["Cemail"])){
 				//jah oli tühi
 				$create_emailerror = "E-mail is required";
-			}	
+			}else{
+				$Cemail = test_input($_POST["Cemail"]);
+			}
 			if(empty($_POST["Cusername"])){
 				//username väli on tühi
 				$create_usererror = "Username is required";
-			}	
+			}else{
+				$Cusername = test_inpu($_POST["Cusername"]);
+			}
 			//kas create password on tühi
 			if(empty($_POST["Cpassword"])){
 				//jah oli tühi
 				$create_passworderror = "Password is required";
-			}	
-			else{
+			}else{
 				//kontrollib et parool oleks rohkem kui 8 sümbolit
 				if(strlen($_POST["Cpassword"]) < 8 ){
-				$create_passworderror = "Must be longer than 8 symbols";
+					$create_passworderror = "Must be longer than 8 symbols";
+				}else{
+					$Cpassword = test_input($_POST["Cpassword"]);
 				}
-				if($_POST["Cpassword"] !== $_POST["repassword"]){
-					//kui parool ei võrdu kordusparooliga lükkab errori ette
-					$create_passwordAerror = "Your password does not match the password entered above";
-				}
+				
 			}
+			
+			if($_POST["Cpassword"] !== $_POST["repassword"]){
+				//kui parool ei võrdu kordusparooliga lükkab errori ette
+				$create_passwordAerror = "Your password does not match the password entered above";
+			}
+			
 		}
+	}
+		
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;	
 	}
 ?>
 
@@ -68,7 +91,7 @@ $file_name = "login.php";
 	<h2>Login</h2>
 	<p>* required field.</p>
 	<form action="login.php" method="post">
-	<input name="email" type="email" placeholder="E-mail"> * <?php echo $email_error?><br><br>
+	<input name="email" type="email" placeholder="E-mail" value="<?php echo $email ?>"> * <?php echo $email_error?><br><br>
 	<input name="password" type="password" placeholder="password"> * <?php echo $password_error?><br><br>
 	<input name="Login" type="submit" value="Login"> <br><br>
 	</form>
@@ -76,8 +99,8 @@ $file_name = "login.php";
 	<h2>Create user</h2>
 	<p>* required field.</p>
 	<form action="login.php" method="post">
-	<input name="Cusername" type="text" placeholder="Username"> * <?php echo $create_usererror?><br><br>
-	<input name="Cemail" type="email" placeholder="E-mail"> * <?php echo $create_emailerror?><br><br>
+	<input name="Cusername" type="text" placeholder="Username" value="<?php echo $Cusername ?>"> * <?php echo $create_usererror?><br><br>
+	<input name="Cemail" type="email" placeholder="E-mail" value="<?php echo $Cemail ?>"> * <?php echo $create_emailerror?><br><br>
 	<input name="Cpassword" type="password" placeholder="password"> * <?php echo $create_passworderror?><br><br>
 	<input name="repassword" type="password" placeholder="password again"> * <?php echo $create_passwordAerror?><br><br>
 	<input name="Create" type="submit" value="Create"><br><br>
