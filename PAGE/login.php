e469c944055fcec9459b452e9703da108c458e29
Rimo Esko <rimoesk@greeny.cs.tlu.ee>
2015-09-18 13:13:31 +0300

    login and create changed

--- PAGE/login.php
+++ PAGE/login.php
@@ -8,49 +8,52 @@
 	$create_emailerror = "";
 	$create_passworderror = "";
 	$repassworderror = "";
-	$create_password2error = "";
+	$create_passwordAerror = "";
 	$create_usererror = "";
 	
 	//kontrollin kas keegi vajutas nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
 		
-		//kas e-post on tühi
-		if(empty($_POST["email"])){
-			//jah oli tühi
-			$email_error = "E-mail is required";
+		if(isset($_POST["Login"])){
+		
+			//kas e-post on tühi
+			if(empty($_POST["email"])){
+				//jah oli tühi
+				$email_error = "E-mail is required";
+			}
+			//kas password on tühi
+			if(empty($_POST["password"])){
+				//jah oli tühi
+				$password_error = "Password is required";
+			}
 		}
-		//kas password on tühi
-		if(empty($_POST["password"])){
-			//jah oli tühi
-			$password_error = "Password is required";
-		}
-	}
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		//kas create email on tühi
-		if(empty($_POST["Cemail"])){
-			//jah oli tühi
-			$create_emailerror = "E-mail is required";
-		}	
-		if(empty($_POST["Cusername"])){
-			//username väli on tühi
-			$create_usererror = "Username is required";
-		}	
-		//kas create password on tühi
-		if(empty($_POST["Cpassword"])){
-			//jah oli tühi
-			$create_passworderror = "Password is required";
-		}	else{
+		elseif(isset($_POST["Create"])){
+			//kas create email on tühi
+			if(empty($_POST["Cemail"])){
+				//jah oli tühi
+				$create_emailerror = "E-mail is required";
+			}	
+			if(empty($_POST["Cusername"])){
+				//username väli on tühi
+				$create_usererror = "Username is required";
+			}	
+			//kas create password on tühi
+			if(empty($_POST["Cpassword"])){
+				//jah oli tühi
+				$create_passworderror = "Password is required";
+			}	
+			else{
 				//kontrollib et parool oleks rohkem kui 8 sümbolit
-			if(strlen($_POST["Cpassword"]) < 8 ){
-			$create_passworderror = "Must be longer than 8 symbols";
-			}
-			if($_POST["Cpassword"] !== $_POST["repassword"]){
-				//kui parool ei võrdu kordusparooliga lükkab errori ette
-				$create_password2error = "Your password does not match the password entered above";
+				if(strlen($_POST["Cpassword"]) < 8 ){
+				$create_passworderror = "Must be longer than 8 symbols";
+				}
+				if($_POST["Cpassword"] !== $_POST["repassword"]){
+					//kui parool ei võrdu kordusparooliga lükkab errori ette
+					$create_passwordAerror = "Your password does not match the password entered above";
+				}
 			}
 		}
 	}
-
 ?>
 
 <?php
@@ -60,30 +63,24 @@ $file_name = "login.php";
 
 <?php require_once("../header.php");?>
 
-	<style>.error {color: #FF0000;}</style>
+
 	<h3>Mõtlesin siis teha lehe kuhu inimesed saavad teha postitusi erinevate teemade alla ja teised saavad siis kommenteerida, põhimõtteliselt väga lihtsustatud reddit</h3>
 	<h2>Login</h2>
-	<p><span class="error">* required field.</span></p>
+	<p>* required field.</p>
 	<form action="login.php" method="post">
-	<input name="email" type="email" placeholder="E-mail"> 
-	<span class="error">* <?php echo $email_error?></span> <br><br>
-	<input name="password" type="password" placeholder="password"> 
-	<span class="error">* <?php echo $password_error?></span> <br><br>
-	<input type="submit" value="Login"> <br><br>
+	<input name="email" type="email" placeholder="E-mail"> * <?php echo $email_error?><br><br>
+	<input name="password" type="password" placeholder="password"> * <?php echo $password_error?><br><br>
+	<input name="Login" type="submit" value="Login"> <br><br>
 	</form>
 
 	<h2>Create user</h2>
-	<p><span class="error">* required field.</span></p>
+	<p>* required field.</p>
 	<form action="login.php" method="post">
-	<input name="Cusername" type="text" placeholder="Username"> 
-	<span class="error">* <?php echo $create_usererror?></span> <br><br>
-	<input name="Cemail" type="email" placeholder="E-mail"> 
-	<span class="error">* <?php echo $create_emailerror?></span> <br><br>
-	<input name="Cpassword" type="password" placeholder="password"> 
-	<span class="error">* <?php echo $create_passworderror?></span> <br><br>
-	<input name="repassword" type="password" placeholder="password again"> 
-	<span class="error">* <?php echo $create_password2error?> <br><br>
-	<input type="submit" value="Create"> <br><br>
+	<input name="Cusername" type="text" placeholder="Username"> * <?php echo $create_usererror?><br><br>
+	<input name="Cemail" type="email" placeholder="E-mail"> * <?php echo $create_emailerror?><br><br>
+	<input name="Cpassword" type="password" placeholder="password"> * <?php echo $create_passworderror?><br><br>
+	<input name="repassword" type="password" placeholder="password again"> * <?php echo $create_passwordAerror?><br><br>
+	<input name="Create" type="submit" value="Create"><br><br>
 	</form>
 	
 <?php require_once("../footer.php");?>
